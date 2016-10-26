0893b05193ee117531e556b34c8a1c85784c1c11
aswin vp <aswinprabhakaran@gmail.com>
2016-10-26 23:28:13 +0530

    Circular array roation

---
+++ HackerRank/Circular_Array_Rotation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Circular_Array_Rotation
+    {
+        static Circular_Array_Rotation()
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            int q = Convert.ToInt32(tokens_n[2]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            for (int a0 = 0; a0 < q; a0++)
+            {
+                int m = Convert.ToInt32(Console.ReadLine());
+                int nm = (n - (k % n) + m) % n;
+                Console.WriteLine(a[nm]);
+            }
+        }
+    }
+}
