316896494fe531948bce605f875f27bddebd1074
aswin vp <aswinprabhakaran@gmail.com>
2016-11-05 00:27:31 +0530

    Kangaroo readme

---
+++ Angry Professor/README.md
@@ -0,0 +1,10 @@
+﻿# Angry Professor
+A Discrete Mathematics professor has a class of  students. Frustrated with their lack of discipline, he decides to cancel class if fewer than  students are present when class starts.
+
+Given the arrival time of each student, determine if the class is canceled.
+
+Input Format
+
+The first line of input contains , the number of test cases.
+
+Each test case consists of two lines. The first line has two space-separated integers,  (students in the class) and (the cancelation threshold). The second line contains  space-separated integers () describing the arrival times for each student.
---
+++ Kangaroo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangaroo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] tokens_x1 = Console.ReadLine().Split(' ');
+            int x1 = Convert.ToInt32(tokens_x1[0]);
+            int v1 = Convert.ToInt32(tokens_x1[1]);
+            int x2 = Convert.ToInt32(tokens_x1[2]);
+            int v2 = Convert.ToInt32(tokens_x1[3]);
+            if (checkIfMeatsAtSameTime(x1, x2, v1, v2))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+        public static bool checkIfMeatsAtSameTime(int x1, int x2, int v1, int v2)
+        {
+            if ((x1 < x2) && (v1 <= v2) || (x1 > x2) && (v1 >= v2))
+                return false;
+            if ((x2 - x1) % (v1 - v2) == 0)
+                return true;
+            return false;
+        }
+    }
+}
