8beedde0d6075f22903801d928e84e512201311b
aswin vp <aswinprabhakaran@gmail.com>
2016-12-16 10:10:04 +0530

    AlphabetShuft

---
+++ AlphabetShift/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inString = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            string outString = "";
+
+            foreach (char c in inString)
+            {
+                if (65 <= c && c <= 90)
+                    outString += Convert.ToChar(((c - 65 + n) % 26) + 65);
+                else if (97 <= c && c <= 122)
+                    outString += Convert.ToChar(((c - 97 + n) % 26) + 97);
+                else
+                    outString += c;
+            }
+            Console.WriteLine(outString);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AlphabetShift/README.md
@@ -0,0 +1,19 @@
+﻿#AlphabetShift
+
+a program to replace all characters in a string with alphabets shifeted n times
+
+###Input Format
+first line string
+second line n
+
+###Constraints
+
+###Output Format
+
+
+###Sample Input 0
+abcxyz_ABCXYZ
+2
+
+###Sample Output 0
+cdezab_CDEZAB
