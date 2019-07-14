b87e1f7141275787e3398cd27051af45463c1531
aswin vp <aswinprabhakaran@gmail.com>
2019-07-14 23:50:46 +0530

    Breaking the Records

---
+++ Apple and Orange/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Apple_and_Orange
+{
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
+        int countApple =0, countOrange = 0;
+
+        for (int i = 0; i < apples.Length; i++)
+        {
+            int position = a + apples[i];
+            if (s <= position && position <= t)
+                countApple++;
+        }
+
+        for (int i = 0; i < oranges.Length; i++)
+        {
+            int position = b + oranges[i];
+            if (s <= position && position <= t)
+                countOrange++;
+        }
+
+        Console.WriteLine(countApple);
+        Console.WriteLine(countOrange);
+        Console.ReadLine();
+    }
+
+    static void Main(string[] args)
+    {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
